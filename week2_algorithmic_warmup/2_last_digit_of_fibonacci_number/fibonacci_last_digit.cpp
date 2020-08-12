98365f3a1f8917efcf7e302af9c3f54b3a3bd24d
lalitkumar-123 <68746773+lalitkumar-123@users.noreply.github.com>
2020-08-12 21:59:51 +0530

    Add files via upload

---
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <assert.h>
+
+int get_fibonacci_last_digit_fast(long long n) {
+    if (n <= 1)
+        return n;
+
+    int previous = 0;
+    int current = 1;
+
+    for (int i = 0; i < n - 1; ++i) {
+        int tmp_previous = previous % 10;
+        previous = current % 10;
+        current = tmp_previous + current % 10;
+    }
+    return current % 10;
+}
+
+// void test_solution() {
+//     assert(get_fibonacci_last_digit_fast(3), 2);
+//     assert(get_fibonacci_last_digit_fast(3), 2);
+//     for (int n = 0; n < 20; ++n)
+//         assert(get_fibonacci_last_digit_fast(n) == get_fibonacci_last_digit_naive(n));
+// }
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    // test_solution();
+    int c = get_fibonacci_last_digit_fast(n);
+    std::cout << c << '\n';
+}
---
+++ week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int fibonacci_sum_squares_naive(long long n)
+{
+    if (n <= 1)
+    {
+       return n;
+    }
+    else
+    {
+        long long a = 0;
+        long long b  = 1;
+        long long c;
+        long long sum = 1;
+
+        for (long long i = 2; i <= n ; i++)
+        {
+            c = a + b;
+            sum = sum + (c*c);
+            a = b;
+            b = c;
+        }
+        return sum % 10;
+    }
+}
+
+int main() {
+    long long n = 0;
+    std::cin >> n;
+    std::cout << fibonacci_sum_squares_naive(n);
+}
